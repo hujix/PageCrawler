c5516d518d35e4caa4a3cde4c11960143fe3b164
Hu.Sir <1668819658@qq.com>
2024-04-02 22:05:19 +0800

    :sparkles: 添加浏览器结果统一处理

--- adapter/base_adapter.py
+++ adapter/base_adapter.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+from adapter.utils import clean_html, parse_meta
 from models import CrawlerResult, CrawlerRequest
 
 
@@ -17,8 +18,26 @@ class BaseCrawler(ABC):
         raise NotImplementedError
 
     @abstractmethod
+    async def initialize(self) -> None:
+        """
+        初始化
+        """
+        raise NotImplementedError
+
+    @abstractmethod
     async def close(self) -> None:
         """
         关闭资源
         """
         raise NotImplementedError
+
+    @classmethod
+    async def _post_process_browser_result(cls, page, adapter: str, item: CrawlerRequest) -> CrawlerResult:
+        title = await page.title()
+        html = await page.content()
+
+        html = clean_html(html, item.clean)
+
+        _, keywords, description = parse_meta(html)
+        return CrawlerResult(url=item.url, title=title, keywords=keywords, html=html,
+                             description=description, adapter=adapter)
