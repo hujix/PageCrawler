0f7ca326e9486b1ffd15d9e967ae29df97667f1b
Hu.Sir <1668819658@qq.com>
2024-05-21 21:52:21 +0800

    ✨ 初始化nodejs项目结构

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import antfu from "@antfu/eslint-config";
+
+export default antfu({
+  stylistic: {
+    semi: true,
+    indent: 2,
+    quotes: "double",
+  },
+  typescript: true, // enable TypeScript support
+});
